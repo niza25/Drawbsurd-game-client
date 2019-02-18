a946931809c640e9fd07f1fe53a3d84b047899cb
Izabela <izabela.nowosada@gmail.com>
2019-02-18 14:38:55 +0100

    Add working canvas

---
+++ src/components/games/canvas.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import CanvasDraw from "react-canvas-draw";
+
+export default class CanvasToDraw extends React.Component {
+
+  state = {
+    color: "#ffc600",
+    width: 800,
+    height: 800,
+    brushRadius: 4,
+    lazyRadius: 5
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div>
+          <button
+            onClick={() => {
+              localStorage.setItem(
+                "savedDrawing",
+                this.saveableCanvas.getSaveData()
+              );
+            }}
+          >
+            Save
+          </button>
+          <button
+            onClick={() => {
+              this.saveableCanvas.clear();
+            }}
+          >
+            Clear
+          </button>
+          <button
+            onClick={() => {
+              this.saveableCanvas.undo();
+            }}
+          >
+            Undo
+          </button>
+        </div>
+        <div>
+          <label>Width:</label>
+          <input
+            type="number"
+            value={this.state.width}
+            onChange={e =>
+              this.setState({ width: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <div>
+          <label>Height:</label>
+          <input
+            type="number"
+            value={this.state.height}
+            onChange={e =>
+              this.setState({ height: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <div>
+          <label>Brush-Radius:</label>
+          <input
+            type="number"
+            value={this.state.brushRadius}
+            onChange={e =>
+              this.setState({ brushRadius: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <div>
+          <label>Lazy-Radius:</label>
+          <input
+            type="number"
+            value={this.state.lazyRadius}
+            onChange={e =>
+              this.setState({ lazyRadius: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <div>
+          <CanvasDraw
+            ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+            brushColor={this.state.color}
+            brushRadius={this.state.brushRadius}
+            lazyRadius={this.state.lazyRadius}
+            canvasWidth={this.state.width}
+            canvasHeight={this.state.height}
+          />
+        </div>
+      </div>
+    )
+
+  }
+
+}
