768cbb6fd5cbbdfc3bbd6e532ac2643e11766e3d
LeoniePeters <leoniepeters@gmail.com>
2019-02-20 15:53:22 +0100

    add colorpicker and some styling

--- src/components/games/CanvasToDraw.js
+++ src/components/games/CanvasToDraw.js
@@ -4,67 +4,60 @@ import { updateGameData } from '../../actions/games'
 import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
 
+import { CirclePicker } from 'react-color';
+import './CanvasToDraw.css'
+
 class CanvasToDraw extends PureComponent {
 
   state = {
     color: '#660066',
-    width: 700,
-    height: 600,
     brushRadius: 2,
-    lazyRadius: 1
+    background: '#660066'
   }
+
+  handleChangeComplete = (color) => {
+    this.setState({
+      background: color.hex,
+      color: color.hex
+    });
+  };
 
   saveDrawing = () => {
     localStorage.setItem(
       "savedDrawing",
       this.saveableCanvas.getSaveData()
-      
+
     );
 
     const { game, updateGameData } = this.props
     const canvas = localStorage.getItem("savedDrawing");
-    
+
     updateGameData(game.id, canvas)
   }
 
-  render() {
-    // document.getElementById('canvasToDraw').addEventListener("click", this.saveDrawing);
 
+  render() {
     return (
       <div>
         <div>
-          <Button style={{backgroundColor: "#ff9900"}}
+          <CirclePicker className='colorpicker'
+            color={this.state.background}
+            onChangeComplete={this.handleChangeComplete}
+          />
+          <Button
+          style={{ backgroundColor: "#d32f2f", margin: 20 }}
             onClick={() => {
               this.saveableCanvas.clear();
             }}>
             Clear
           </Button>
-          <Button style={{backgroundColor: "#ff9900"}}
+          <Button
+          style={{ backgroundColor: "#ff9900", margin: 20 }}
             onClick={() => {
               this.saveableCanvas.undo();
             }}>
             Undo
           </Button>
-        </div>
-        <div>
-          <label>Change width</label>
-          <input
-            type="number"
-            value={this.state.width}
-            onChange={e =>
-              this.setState({ width: parseInt(e.target.value, 10) })
-            }
-          />
-        </div>
-        <div>
-          <label>Change height</label>
-          <input
-            type="number"
-            value={this.state.height}
-            onChange={e =>
-              this.setState({ height: parseInt(e.target.value, 10) })
-            }
-          />
         </div>
         <div>
           <label>Brush's size</label>
@@ -81,12 +74,12 @@ class CanvasToDraw extends PureComponent {
             ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
             brushColor={this.state.color}
             brushRadius={this.state.brushRadius}
-            lazyRadius={this.state.lazyRadius}
-            canvasWidth={this.state.width}
-            canvasHeight={this.state.height}
+            lazyRadius={1}
+            canvasWidth={700}
+            canvasHeight={600}
           />
         </div>
-        
+
       </div>
     )
 
