e79d700391aee402f63d5a5640a62058dca55248
LeoniePeters <leoniepeters@gmail.com>
2019-02-21 16:35:48 +0100

    add url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'https://murmuring-inlet-92645.herokuapp.com/' // 'http://localhost:4000'
 
 export const localStorageJwtKey = 'currentUserJwt'
