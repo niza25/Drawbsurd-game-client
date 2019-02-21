f507580be463af79223327192f662731e14f2ded
Izabela <izabela.nowosada@gmail.com>
2019-02-21 16:58:16 +0100

    Adjust the link

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://murmuring-inlet-92645.herokuapp.com/' // 'http://localhost:4000'
+export const baseUrl = 'https://murmuring-inlet-92645.herokuapp.com' // 'http://localhost:4000'
 
 
 export const localStorageJwtKey = 'currentUserJwt'
