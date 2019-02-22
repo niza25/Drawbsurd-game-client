378494bf7ff8f5bc3cff9a63c4e737e82dbaff3c
LeoniePeters <45720401+LeoniePeters@users.noreply.github.com>
2019-02-22 13:35:13 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:4000' //'https://murmuring-inlet-92645.herokuapp.com/' 
+export const baseUrl = 'https://murmuring-inlet-92645.herokuapp.com' //'http://localhost:4000'
 
 
 
