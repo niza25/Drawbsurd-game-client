d45d1ac2f683a2c1c1ce07ebbfc98bb22d872cbe
LeoniePeters <leoniepeters@gmail.com>
2019-02-22 15:33:08 +0100

    leave out timer

--- src/components/games/CanvasToDisplay.js
+++ src/components/games/CanvasToDisplay.js
@@ -15,8 +15,6 @@ export default class CanvasToDisplay extends PureComponent {
                 hideGrid
                 ref={canvasDraw => (this.loadableCanvas = canvasDraw)}
                 saveData={this.props.canvasDisplay}
-                canvasWidth={700}
-            canvasHeight={600}
             />
 
         )
